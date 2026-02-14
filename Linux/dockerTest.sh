33d2d5bad4888704b58ae7b5c6ff66c8284991af
river <git@riverrooks.art>
2026-02-13 21:06:13 -0600

    commented out proccess code

--- Linux/dockerTest.sh
+++ Linux/dockerTest.sh
@@ -9,6 +9,6 @@ echo -e "trying on ubuntu \n"
 	docker container rm quickstart-ubuntu
 	docker container run --name quickstart-ubuntu -v ./:/home -it ub-test:1 /bin/bash -c "/home/quickstart.sh && /bin/bash"
 
-echo -e "trying on arch \n"
-	docker container rm quickstart-arch
-	docker container run --name quickstart-arch -v ./:/home -it ub-test:1 /bin/bash
+# echo -e "trying on arch \n"
+# 	docker container rm quickstart-arch
+# 	docker container run --name quickstart-arch -v ./:/home -it ub-test:1 /bin/bash
