7b1aed5d6d3fb01f0efb74f40ce53d829d2fe7da
river <git@riverrooks.art>
2026-02-13 22:42:43 -0600

    added support for no input loops all

--- Linux/dockerTest.sh
+++ Linux/dockerTest.sh
@@ -1,9 +1,7 @@
 #! /usr/bin/sh
 buildImage(){
-	# if [ -n "$(docker image ls 2> /dev/null| grep $1-test )" ]; then
-		echo building $1
-		docker build -f Dockerfile.$1-test -t $1-test:latest .
-	# fi
+	echo building $1
+	docker build -f Dockerfile.$1-test -t $1-test:latest .
 }
 
 tryDistro(){
@@ -12,15 +10,27 @@ tryDistro(){
 		# docker container kill $1-test
 		docker container rm -f $1-test
 	fi
-	docker container run --name $1-test -v ./:/home -it $1-test:latest /bin/bash -c "/home/quickstart.sh && /bin/bash"
+	docker container run --name $1-test -v ./:/home $isIneractive $1-test:latest /bin/bash -c "/home/quickstart.sh && /bin/bash"
 }
 
-# workingDistros=("ubuntu" "arch" "fedora" "alpine")
+runDistro(){
+	echo $1
+	buildImage $1
+	tryDistro $1
+}
 
-# for distro in "${workingDistros[1]}"; do
-for distro in "$1"; do
-	echo $distro
-	buildImage $distro
-	tryDistro $distro
-done
+isInteractive = "-it"
+
+workingDistros=("ubuntu" "arch" "fedora" "alpine")
+
+if [ -z $1 ]; then 
+	echo no distro specified, trying all
+	for distro in "${workingDistros[@]}"; do
+		isInteractive = ""
+		runDistro $distro
+		read
+	done
+else
+	runDistro $1
+fi
 
