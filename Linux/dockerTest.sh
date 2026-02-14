3c7fbdef64982c7f72e4ae504de83baabac08be0
river <git@riverrooks.art>
2026-02-13 21:50:33 -0600

    fixed syntax change

--- Linux/dockerTest.sh
+++ Linux/dockerTest.sh
@@ -4,17 +4,17 @@ if [ -z "$(docker image ls 2> /dev/null| grep ub-test )" ]; then
 	echo building ubuntu
 	docker build -f Dockerfile.ubuntu-test -t ubuntu-test:latest .
 	# echo building arch
-	# docker build -f Dockerfile.arch-test -t arch-test:latest .
+	docker build -f Dockerfile.arch-test -t arch-test:latest .
 fi
 
 tryDistro(){
 	echo -e "trying on ubuntu \n"
-	if [ -n "(docker container ps -a | grep $1)" ]; then
-		docker container kill quickstart-$1
-		docker container rm -f quickstart-$1
+	if [ -z "(docker container ps -a | grep $1)" ]; then
+		docker container kill $1-test
+		docker container rm -f $1-test
 	fi
-	docker container run --name quickstart-$1 -v ./:/home -it $1-test:latest /bin/bash -c "/home/quickstart.sh && /bin/bash"
+	docker container run --name $1-test -v ./:/home -it $1-test:latest /bin/bash -c "/home/quickstart.sh && /bin/bash"
 }
 
 	tryDistro ubuntu
-	# tryDistro arch
+	tryDistro arch
