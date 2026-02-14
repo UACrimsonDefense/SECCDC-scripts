4b9461ce4bc006c0a4bdfff88e4176a23afaafec
river <git@riverrooks.art>
2026-02-13 20:30:28 -0600

    removes previous name

--- Linux/dockerTest.sh
+++ Linux/dockerTest.sh
@@ -6,4 +6,5 @@ if [ -z "$(docker image ls 2> /dev/null| grep ub-test )" ]; then
 fi
 
 echo -e "trying on ubuntu \n"
+docker container rm quickstart-ubuntu
 docker container run --name quickstart-ubuntu -v ./:/home -it ub-test:1 /home/quickstart.sh
