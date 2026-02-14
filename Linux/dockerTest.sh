9e819e7997b568834e082255dc6b98e5505e00b0
river <git@riverrooks.art>
2026-02-13 22:26:30 -0600

    fixed arch

--- Linux/dockerTest.sh
+++ Linux/dockerTest.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/sh
 buildImage(){
-	if [ -z "$(docker image ls 2> /dev/null| grep $1-test )" ]; then
+	# if [ -n "$(docker image ls 2> /dev/null| grep $1-test )" ]; then
 		echo building $1
 		docker build -f Dockerfile.$1-test -t $1-test:latest .
-	fi
+	# fi
 }
 
 tryDistro(){
